fad474dc5f8d2ea472d990ba7f34a72c004eed6f
Victor Miranda <mirandaguedes.victor@gmail.com>
2021-12-28 11:36:01 -0300

    Init

---
+++ config/initializers/inflections.rb
@@ -0,0 +1,8 @@
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+   inflect.irregular 'hero', 'heroes'
+end
+
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
