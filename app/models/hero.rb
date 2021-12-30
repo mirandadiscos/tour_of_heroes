b2f094543487cb8adc78cd4e774a4594e89a7641
Victor Miranda <mirandaguedes.victor@gmail.com>
2021-12-30 11:06:52 -0300

    Adiciona busca na lsita de heroes

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -3,4 +3,5 @@ class Hero < ApplicationRecord
 
     scope :sorted_by_name, -> { order(:name) }
     #scope :sorted_desc_by_name, -> { order(name: :desc) }
+    scope :search, -> (term) { where('LOWER(name)LIKE ?', "%#{term.downcase}%") if term.present? }
 end
