284df91030d73450aca61759327222935bba5d23
Victor Miranda <mirandaguedes.victor@gmail.com>
2021-12-30 13:47:34 -0300

    Altera CRUD para token no banco de dados

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -4,12 +4,12 @@ module Authenticable
     def authenticate_with_token
         @token ||= request.headers['Authorization']
         unless valid_token?
-            render json: { errors: 'Voce não tem autorização para esta operação' }, status: :unauthorized 
+            render json: { errors: 'Forneça um header Authorization para se identificar(qualquer um com pelo menos 10 caracteres' }, status: :unauthorized 
         end
     end
 
     def valid_token?
-        @token.present? && @token == Rails.application.credentials.token
+        @token.present? && @token.size  >= 10
     end
 
 end
--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,7 +1,9 @@
 class Hero < ApplicationRecord
     validates :name, :token, presence: true
 
+    
     scope :sorted_by_name, -> { order(:name) }
     #scope :sorted_desc_by_name, -> { order(name: :desc) }
-    scope :search, -> (term) { where('LOWER(name)LIKE ?', "%#{term.downcase}%") if term.present? }
+    scope :search, -> (term) { where('LOWER(name)LIKE ?', "%#{term.downcase}%") if term.present?  }
+    scope :by_token, -> (token) { where(token: token) }
 end
